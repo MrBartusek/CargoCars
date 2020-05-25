299aac6ce1e2ed0ef5c04f7021e6f00a16c48d55
MrBartusek <bartusekcraft@gmail.com>
2020-05-26 00:42:31 +0200

    Minimal setup with header bootstrap and scss

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../styles/App.scss';
+
+import Header from './header'
+
+export default class App extends React.Component 
+{
+  render() 
+  {
+    return (
+      <Header />
+    );
+  }
+}
