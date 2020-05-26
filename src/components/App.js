c95f96e9d00c366169d019506e988019d373c12b
MrBartusek <bartusekcraft@gmail.com>
2020-05-26 16:17:05 +0200

    Revert "Rename files to upper case"
    This reverts commit db001e1175d60a14d4974f8447465d3d02e1cdfc.

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,8 @@
 import React, { Fragment } from 'react';
 import '../styles/App.scss';
-import Header from './Header'
-import Footer from './Footer'
+import {Container, Row, Col,} from 'react-bootstrap'
+import Header from './header'
+import Footer from './footer'
 
 
 export default class App extends React.Component 
