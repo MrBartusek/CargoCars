9574c0937c1fe9adbbc9d71458ae78bf9334f9a0
MrBartusek <MrBartusek@users.noreply.github.com>
2020-05-27 11:23:06 +0000

    deploy: e93b927c9fc6ee14301105c4aa86baeb8a482484

---
+++ precache-manifest.2deeb9e5500f31b25c2f92438aaf6c82.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8db8c8f473f8f8432ab7cf650d0196c7",
+    "url": "/CargoCars/index.html"
+  },
+  {
+    "revision": "18a0873924159fdb8d2f",
+    "url": "/CargoCars/static/css/main.b6f1eb6c.chunk.css"
+  },
+  {
+    "revision": "8db62b6dc79d6733bcba",
+    "url": "/CargoCars/static/js/2.d23cda5e.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/CargoCars/static/js/2.d23cda5e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18a0873924159fdb8d2f",
+    "url": "/CargoCars/static/js/main.09c63087.chunk.js"
+  },
+  {
+    "revision": "081cbedf2b06acb94ac2",
+    "url": "/CargoCars/static/js/runtime-main.05c33b4c.js"
+  }
+]);
