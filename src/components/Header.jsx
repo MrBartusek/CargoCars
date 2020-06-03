000953d1c5fbed8da225324817141e0707191579
MrBartusek <bartusekcraft@gmail.com>
2020-06-03 20:19:04 +0200

    Remove links from header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,12 +10,7 @@ export default class Header extends Component
 			<header>
 				<Navbar bg="dark" variant="dark">
 					<Navbar.Brand href="#home">Cargo Cars</Navbar.Brand>
-					<Nav className="mr-auto">
-						<Nav.Link href="#home">Home</Nav.Link>
-						<Nav.Link href="#collections">Collections</Nav.Link>
-						<Nav.Link href="#about">About</Nav.Link>
-					</Nav>
-					<Nav className="justify-content-end">
+					<Nav className="justify-content-end ml-auto">
 						<Nav.Link href="https://github.com/MrBartusek/CargoCars" target='_blank' rel="noopener noreferrer">
 							<FaGithub color="white" size="1.2em" />
 						</Nav.Link> 
